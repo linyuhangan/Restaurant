aa229aeee9c957d379b23f414ed162810034b53e
拾弎彡矢 <1771601311@qq.com>
2022-07-18 14:52:18 +0800

    提交

--- src/main/java/com/lyhyl/restaurant/Controller/EmployeeController.java
+++ src/main/java/com/lyhyl/restaurant/Controller/EmployeeController.java
@@ -1,18 +1,18 @@
 package com.lyhyl.restaurant.Controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lyhyl.restaurant.Service.EmployeeService;
 import com.lyhyl.restaurant.common.R;
 import com.lyhyl.restaurant.entity.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -62,5 +62,61 @@ public class EmployeeController {
         request.getSession().removeAttribute("employee");
 
     return R.success("成功退出登录");
+    }
+
+    /**
+     * 添加员工
+     * @param request
+     * @param employee
+     * @return
+     *
+     * 返回json用@requestbody接收
+     */
+    @PostMapping
+    public R<String> adduser(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("员工信息",employee.toString());
+
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        //创建时间
+        employee.setCreateTime(LocalDateTime.now());
+        //修改时间
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long empID =(Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empID);
+
+        employee.setUpdateUser(empID);
+
+       employeeService.save(employee);
+
+
+        return R.success("添加成功");
+    }
+
+    /**
+     * 员工信息查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize, String name){
+        //分页构造器
+        Page page1 = new Page(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Employee> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+
+        //过滤条件
+        lambdaQueryWrapper.like(!StringUtils.isEmpty(name),Employee::getName,name);
+
+        //排序条件
+         lambdaQueryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        //执行查询
+        employeeService.page(page1,lambdaQueryWrapper);
+
+        return R.success(page1);
     }
 }
